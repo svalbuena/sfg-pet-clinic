03a04f05ab9ac7f64dc0c4b6452cf1b5e78fd99c
Sergi Valbuena Garrido <sergivalbuena@hotmail.com>
2021-04-01 13:43:18 +0200

    List all Owners on Owner Index Page. Closes #18

--- pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,11 +3,20 @@ package svalbuena.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import svalbuena.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(final OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping("/owners")
     public String showOwners(final Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+        
         return "owners/index";
     }
 }
