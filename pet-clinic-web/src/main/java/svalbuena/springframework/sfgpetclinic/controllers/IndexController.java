c8cd3fb01352f40a9d1f5aef288f23c8ecfdf880
Sergi Valbuena Garrido <sergivalbuena@hotmail.com>
2021-04-01 11:15:18 +0200

    Change RequestMapping to GetMapping on IndexController

--- pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,11 +1,11 @@
 package svalbuena.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping({"", "/", "index", "index.html"})
+    @GetMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
     }
