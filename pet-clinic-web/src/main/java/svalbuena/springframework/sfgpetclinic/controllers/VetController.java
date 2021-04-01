c90445c9e443015b0c98ad8f08af856c1354530a
Sergi Valbuena Garrido <sergivalbuena@hotmail.com>
2021-04-01 13:47:19 +0200

    List all Vets on Vet Index Page. Closes #17

--- pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,11 +3,20 @@ package svalbuena.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import svalbuena.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(final VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping("/vets")
     public String showVets(final Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
