a332ab8c393c5e628e9828c81520b631646aeded
Sergi Valbuena Garrido <sergivalbuena@hotmail.com>
2021-04-02 11:46:18 +0200

    Add PetType in Bootstrap startup. Closes #34

--- pet-clinic-data/src/main/java/svalbuena/springframework/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/svalbuena/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -1,6 +1,8 @@
 package svalbuena.springframework.sfgpetclinic.services.map;
 
+import org.springframework.stereotype.Service;
 import svalbuena.springframework.sfgpetclinic.model.PetType;
 import svalbuena.springframework.sfgpetclinic.services.PetTypeService;
 
+@Service
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {}
--- pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/svalbuena/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,42 +3,57 @@ package svalbuena.springframework.sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import svalbuena.springframework.sfgpetclinic.model.Owner;
+import svalbuena.springframework.sfgpetclinic.model.PetType;
 import svalbuena.springframework.sfgpetclinic.model.Vet;
 import svalbuena.springframework.sfgpetclinic.services.OwnerService;
+import svalbuena.springframework.sfgpetclinic.services.PetTypeService;
 import svalbuena.springframework.sfgpetclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(final OwnerService ownerService, final VetService vetService) {
+    public DataLoader(final OwnerService ownerService,
+                      final VetService vetService,
+                      final PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(final String... args) {
-        ownerService.save(createOwner());
-        ownerService.save(createOwner());
+        petTypeService.save(createPetType("dog"));
+        petTypeService.save(createPetType("cat"));
 
-        vetService.save(createVet());
-        vetService.save(createVet());
+        ownerService.save(createOwner("John", "Howards"));
+        ownerService.save(createOwner("Peter", "Samuels"));
+
+        vetService.save(createVet("Key", "Master"));
+        vetService.save(createVet("Anna", "Limp"));
 
         System.out.println("Loaded!!");
     }
 
-    private Owner createOwner() {
+    private Owner createOwner(final String firstName, final String lastName) {
         final Owner owner = new Owner();
-        owner.setFirstName("OWNER_FIRST_NAME");
-        owner.setLastName("OWNER_LAST_NAME");
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
         return owner;
     }
 
-    private Vet createVet() {
+    private Vet createVet(final String firstName, final String lastName) {
         final Vet vet = new Vet();
-        vet.setFirstName("VET_FIRST_NAME");
-        vet.setLastName("VET_LAST_NAME");
+        vet.setFirstName(firstName);
+        vet.setLastName(lastName);
         return vet;
+    }
+
+    private PetType createPetType(final String name) {
+        final PetType petType = new PetType();
+        petType.setName(name);
+        return petType;
     }
 }
