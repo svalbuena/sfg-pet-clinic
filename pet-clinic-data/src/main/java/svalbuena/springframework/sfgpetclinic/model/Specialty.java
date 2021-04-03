9b1afeba6b98939f8c1565df4a116278c65bbf37
Sergi Valbuena Garrido <sergivalbuena@hotmail.com>
2021-04-03 10:57:12 +0200

    Implement Vet and Specialty JPA. Closes #39

--- pet-clinic-data/src/main/java/svalbuena/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/svalbuena/springframework/sfgpetclinic/model/Specialty.java
@@ -1,5 +1,8 @@
 package svalbuena.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Specialty extends BaseEntity {
     private String description;
 
--- pet-clinic-data/src/main/java/svalbuena/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/svalbuena/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,17 @@
 package svalbuena.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialty", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
